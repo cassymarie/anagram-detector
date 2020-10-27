09c36da61b71cad4f00ed5b75616fb291fe6d9d9
cassymarie <cassy2205@gmail.com>
2020-10-27 17:55:31 -0600

    commit tree ca721ddb9caba78ca51ff5c53d29bcad33e0b7fd

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word, :list, :anagramList
+    
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(list)
+        @list = list
+        @anagramList = []
+        @list.each do |word|
+            if word.split("").sort == @word.split("").sort
+                @anagramList << word
+            end
+        end
+        @anagramList
+    end
+end
+
+
+# detector = Anagram.new('listen')
+# detector.match(["enlists", "google", "inlets", "banana"])
+# detector.match(["gallery", "ballerina", "regally", "clergy", "largely", "leading"])
+
+
+
+
